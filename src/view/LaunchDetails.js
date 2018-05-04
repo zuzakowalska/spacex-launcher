c46eaae9354f505070a8cecd2db9efb1175e9978
Zuzanna Kowalska <zuzanna.kowalska@brandnova.com>
2018-05-04 13:53:38 +0200

    finished header

--- src/view/LaunchDetails.js
+++ src/view/LaunchDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Launch from '../components/Launch.js'
+import '../assets/scss/LaunchDetails.scss';
 
 class LaunchDetails extends Component {
     render() {
@@ -8,7 +9,7 @@ class LaunchDetails extends Component {
 <div className="launch-details">
 <div className="header">
         <div className="back">
-            <img src="" alt="" />
+            <div className="arrow"><div className="arrow__bg"></div><div className="arrow__line"></div></div>
             <h3>go back</h3>
         </div>
         <div className="logo"></div>
